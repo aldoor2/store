51af11465cc4d30b8e0463c85029e6c97178cb97
Alphonse <apuertaj2@gmail.com>
2022-07-19 22:18:44 -0500

    Updated constants with assets and sheet template

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,12 +1,12 @@
 export const INFORMATION = {
-  avatar: "//place-hold.it/256x256",
-  banner: "//place-hold.it/1440x320",
-  title: "Almacency",
-  description: "El almacen de goncy",
+  avatar: "/assets/avatar.jpg",
+  banner: "/assets/banner.jpg",
+  title: "URBN Catering",
+  description: "Galardonada pizza estilo New Haven en Palermo SOHO.",
   phone: "5491141414141",
   sheet:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vThDpi0HoxEO77NELrovO_GD0QDtWEztCi3LoIRvDFCDZAirKvT6hX_ysNewYC05iCsqdpG3FoliYhK/pub?output=csv",
-  color: "orange",
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQyKHkHlToliTiCgMJNa-5iE-eW-PVeD127sY6y6UOH5kIr4A_ah2IzlAOt2dC0tn-VduVoJVKcgcWo/pub?output=csv",
+  color: "teal",
   social: [
     {
       name: "instagram",
